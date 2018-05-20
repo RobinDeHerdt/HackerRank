424debc34b18b20eb01f17e513cca839723505f8
Robin De Herdt <robindh95@gmail.com>
2018-05-20 11:32:27 +0200

    Add the lazy version of the 'magic square forming' solution

--- Algorithms/Implementation/magic-square-forming.py
+++ Algorithms/Implementation/magic-square-forming.py
@@ -1,99 +1,33 @@
 #!/bin/python3
 
 import os
-import copy
 
 
 def formingMagicSquare(matrix):
-    def getHorizontalSums(matrix):
-        horizontal_sums = []
-        for line in matrix:
-            horizontal_sums.append(sum(line))
+    possible_magic_squares = [
+        [[8, 1, 6], [3, 5, 7], [4, 9, 2]],
+        [[6, 1, 8], [7, 5, 3], [2, 9, 4]],
+        [[4, 9, 2], [3, 5, 7], [8, 1, 6]],
+        [[2, 9, 4], [7, 5, 3], [6, 1, 8]],
+        [[8, 3, 4], [1, 5, 9], [6, 7, 2]],
+        [[4, 3, 8], [9, 5, 1], [2, 7, 6]],
+        [[6, 7, 2], [1, 5, 9], [8, 3, 4]],
+        [[2, 7, 6], [9, 5, 1], [4, 3, 8]],
+    ]
 
-        return horizontal_sums
-
-    def getVerticalSums(matrix):
-        vertical_sums = []
-        for i in range(3):
-            line_sum = 0
-            for line in matrix:
-                line_sum += line[i]
-
-            vertical_sums.append(line_sum)
-
-        return vertical_sums
-
-    def getDiagonalSums(matrix):
-        diagonal_sums = []
-
-        index = 0
-        line_sum = 0
-        for line in matrix:
-            line_sum += line[index]
-            index += 1
-
-        diagonal_sums.append(line_sum)
-
-        index = 2
-        line_sum = 0
-        for line in matrix:
-            line_sum += line[index]
-            index -= 1
-
-        diagonal_sums.append(line_sum)
-
-        return diagonal_sums
-
-    def getMissingNumbers(matrix):
-        numbers = list(range(1, 10))
-        for line in matrix:
-            for value in line:
-                if value in numbers:
-                    numbers.remove(value)
-
-        return numbers
-
-    def getDuplicateNumbers(matrix):
-        numbers = []
-        duplicates = []
-        for line in matrix:
-            for value in line:
-                if value in numbers:
-                    duplicates.append(value)
+    totals = []
+    for magic_square in possible_magic_squares:
+        total = 0
+        for possible_row, matrix_row in zip(magic_square, matrix):
+            for i, j in zip(possible_row, matrix_row):
+                if i == j:
                     continue
 
-                numbers.append(value)
+                total += max([i, j]) - min([i, j])
 
-        return duplicates
+        totals.append(total)
 
-    def isValidMatrix(matrix):
-        horizontal_sums = getHorizontalSums(matrix)
-        vertical_sums = getVerticalSums(matrix)
-        diagonal_sums = getDiagonalSums(matrix)
-
-        sums = horizontal_sums + vertical_sums + diagonal_sums
-
-        # Set the first value as the default magic constant
-        magic_constant = sums[0]
-        for i in sums:
-            if i != magic_constant:
-                return False
-
-        return True
-
-    duplicates = getDuplicateNumbers(matrix)
-    missing_numbers = getMissingNumbers(matrix)
-    for line_index, line in enumerate(matrix):
-        for index, value in enumerate(line):
-            if value in duplicates:
-                for i in missing_numbers:
-                    original_matrix = copy.deepcopy(matrix)
-                    matrix[line_index][index] = i
-                    print(matrix)
-                    if isValidMatrix(matrix):
-                        return i - value
-                    else:
-                        matrix = copy.deepcopy(original_matrix)
+    return min(totals)
 
 
 if __name__ == '__main__':
