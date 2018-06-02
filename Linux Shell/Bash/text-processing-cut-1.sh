ce38b33244ce423ae68780f9949e40d768e35b04
Robin De Herdt <robindh95@gmail.com>
2018-06-02 13:50:55 +0200

    Add the 'cut #1' solution

---
+++ Linux Shell/Bash/text-processing-cut-1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+while IFS= read -r line
+do
+    # This should work, but for some reason
+    # it keeps failing at one specific test case.
+    # echo ${line:2:1}
+
+    # This however, does work for all test cases.
+    echo $line | cut -c3
+done
