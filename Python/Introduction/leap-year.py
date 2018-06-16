7604e1e6ecaf91c577e09e4e223a868f324f5403
Robin De Herdt <robindh95@gmail.com>
2018-06-16 15:02:28 +0200

    Add the 'leap year' solution

---
+++ Python/Introduction/leap-year.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+
+            return False
+
+        return True
+
+    return False
