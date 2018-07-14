996c37c367c0937a03ce2e14e78b892fd1642ae2
Robin De Herdt <robindh95@gmail.com>
2018-07-14 11:31:15 +0200

    Add the 'string validators' solution

---
+++ Python/Strings/string-validators.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+
+def validate_string(s):
+    print(any(char.isalnum() for char in s))
+    print(any(char.isalpha() for char in s))
+    print(any(char.isdigit() for char in s))
+    print(any(char.islower() for char in s))
+    print(any(char.isupper() for char in s))
+
+
+if __name__ == '__main__':
+    s = input()
+
+    validate_string(s)
