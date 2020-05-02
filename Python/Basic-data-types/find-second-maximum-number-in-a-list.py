720ce69dcf20ce98eb7a57d07433837d365164d8
Robin De Herdt <robindh95@gmail.com>
2020-05-02 16:13:18 +0200

    Refactor existing scripts to use the testing module

--- Python/Basic-data-types/find-second-maximum-number-in-a-list.py
+++ Python/Basic-data-types/find-second-maximum-number-in-a-list.py
@@ -1,19 +1,18 @@
 # https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem
-if __name__ == '__main__':
 
-    def find_second_maximum_number_in_list(arr):
-        # Convert the map to a set; only unique values will remain.
-        unique_arr_values = set(arr)
+from Testing import assertions
 
-        # Convert the set to a list, so we can sort it.
-        unique_arr_values = list(unique_arr_values)
-        unique_arr_values.sort()
 
-        return unique_arr_values[-2]
+def find_second_maximum_number_in_list(arr):
+    # Convert the map to a set; only unique values will remain.
+    unique_arr_values = set(arr)
 
-    def assert_equals(expected, actual):
-        assert expected == actual, "Expected {0} but got {1}".format(expected, actual)
-        print("\033[92m [SUCCESS] {0} equals {1}".format(expected, actual))
+    # Convert the set to a list, so we can sort it.
+    unique_arr_values = list(unique_arr_values)
+    unique_arr_values.sort()
 
-    assert_equals(5, find_second_maximum_number_in_list([2, 3, 6, 6, 5]))
-    assert_equals(4, find_second_maximum_number_in_list([7, 0, 4, 2, 3]))
+    return unique_arr_values[-2]
+
+
+assertions.assert_equals(5, find_second_maximum_number_in_list([2, 3, 6, 6, 5]))
+assertions.assert_equals(4, find_second_maximum_number_in_list([7, 0, 4, 2, 3]))
--- Python/Basic-data-types/finding-the-percentage.py
+++ Python/Basic-data-types/finding-the-percentage.py
@@ -1,17 +1,14 @@
 # https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem
-if __name__ == '__main__':
 
-    def find_student_average(student_marks, last_student_name):
-        last_student_scores = student_marks[last_student_name]
-        average = sum(last_student_scores) / len(last_student_scores)
-        return '{:.2f}'.format(average)
+from Testing import assertions
 
-    def assert_equals(expected, actual):
-        assert expected == actual, "Expected {0} but got {1}".format(expected, actual)
-        print("\033[92m [SUCCESS] {0} equals {1}".format(expected, actual))
+def find_student_average(student_marks, last_student_name):
+    last_student_scores = student_marks[last_student_name]
+    average = sum(last_student_scores) / len(last_student_scores)
+    return '{:.2f}'.format(average)
 
-    example1 = dict([['Harsh', [25, 26.5, 28]], ['Anurag', [26, 28, 30]]])
-    assert_equals("26.50", find_student_average(example1, "Harsh"))
+example1 = dict([['Harsh', [25, 26.5, 28]], ['Anurag', [26, 28, 30]]])
+assertions.assert_equals("26.50", find_student_average(example1, "Harsh"))
 
-    example2 = dict([['Krishna', [67, 68, 69]], ['Arjun', [70, 98, 63]], ['Malika', [52, 56, 60]]])
-    assert_equals("56.00", find_student_average(example2, "Malika"))
+example2 = dict([['Krishna', [67, 68, 69]], ['Arjun', [70, 98, 63]], ['Malika', [52, 56, 60]]])
+assertions.assert_equals("56.00", find_student_average(example2, "Malika"))
